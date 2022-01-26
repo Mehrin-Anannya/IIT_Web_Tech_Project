824734ec21da8aa6c88fdd0c3d78643155fc0029
Mehrin-Anannya <anannya.mehrin@gmail.com>
2022-01-26 14:21:26 +0600

    Removed unnecessary codes.

--- jsp-servlet-jdbc-mysql-crud-example/src/main/java/iit/webtechnology/usermanagement/web/UserServlet.java
+++ jsp-servlet-jdbc-mysql-crud-example/src/main/java/iit/webtechnology/usermanagement/web/UserServlet.java
@@ -22,66 +22,69 @@ import iit.webtechnology.usermanagement.model.User;
 public class UserServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private UserDAO userDAO;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-	/*
-	 * public UserServlet() { this.userDAO = new UserDAO();
-	 * 
-	 * // TODO Auto-generated constructor stub }
+
+	/**
+	 * @see HttpServlet#HttpServlet()
 	 */
-    
-    public void init() {
-		userDAO = new UserDAO();
+
+	public UserServlet() {
+		this.userDAO = new UserDAO();
 	}
 
-    	private void onload(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-			// TODO Auto-generated method stub
-    		doGet(request, response);
-		}
+	private void onload(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		String action = request.getServletPath();
 		System.out.println(action);
-		switch(action) {
-		case "/new": showNewForm(request, response); 
+		switch (action) {
+		case "/new":
+			showNewForm(request, response);
 			break;
-		case "/insert" : try {
+		case "/insert":
+			try {
 				insertUser(request, response);
 			} catch (SQLException e) {
 				e.printStackTrace();
-			} 
+			}
 			break;
-		case "/edit" : 
+		case "/edit":
 			showEditForm(request, response);
 			break;
-		case "/delete" : 
+		case "/delete":
 			try {
 				deleteUser(request, response);
 			} catch (SQLException e) {
 				e.printStackTrace();
-			} 
+			}
 			break;
-		case "/update" : try {
+		case "/update":
+			try {
 				updateUser(request, response);
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
 			break;
-		default: listUsers(request, response);
+		default:
+			listUsers(request, response);
 			break;
 		}
-		
+
 	}
-	private void showNewForm(HttpServletRequest request, HttpServletResponse response) 
+
+	private void showNewForm(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		RequestDispatcher dispatcher = request.getRequestDispatcher("userForm.jsp");
-		dispatcher.forward(request, response);		
+		dispatcher.forward(request, response);
 	}
-	
+
 	private void insertUser(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException {
 		String name = request.getParameter("name");
 		String email = request.getParameter("email");
@@ -90,43 +93,47 @@ public class UserServlet extends HttpServlet {
 		userDAO.insertUser(newUser);
 		response.sendRedirect(request.getContextPath() + "/list");
 	}
-	private void showEditForm(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-		int id = Integer.parseInt(request.getParameter("id")); 
+
+	private void showEditForm(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		int id = Integer.parseInt(request.getParameter("id"));
 		User currentUser = userDAO.selectUser(id);
 		RequestDispatcher dispatcher = request.getRequestDispatcher("userForm.jsp");
 		request.setAttribute("user", currentUser);
 		dispatcher.forward(request, response);
 	}
-	
-	private void deleteUser(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException{
+
+	private void deleteUser(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException {
 		System.out.println(request.getParameter("id"));
-		int id = Integer.parseInt(request.getParameter("id")); 
+		int id = Integer.parseInt(request.getParameter("id"));
 		userDAO.deleteUser(id);
 		response.sendRedirect(request.getContextPath() + "/list");
 	}
-	
-	private void updateUser(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException{
+
+	private void updateUser(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException {
 		String name = request.getParameter("name");
 		String email = request.getParameter("email");
 		String country = request.getParameter("country");
 		int id = Integer.parseInt(request.getParameter("id"));
-		User newUser = new User(id,name, email, country);
+		User newUser = new User(id, name, email, country);
 		userDAO.updateUser(newUser);
 		response.sendRedirect(request.getContextPath() + "/list");
 	}
-	
-	private void listUsers(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	private void listUsers(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		List<User> users = userDAO.selectAllUsers();
 		request.setAttribute("listUser", users);
 		RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
 		dispatcher.forward(request, response);
 	}
-	
-	
+
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
--- jsp-servlet-jdbc-mysql-crud-example/src/main/webapp/index.jsp
+++ jsp-servlet-jdbc-mysql-crud-example/src/main/webapp/index.jsp
@@ -9,7 +9,8 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" 
 integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
-<body>	
+<body>
+	<%-- <jsp: include page= ""/>	 --%>
 	<nav class="navbar navbar-expand-md navbar-light bg-success">
 		<h3 class="text-white">Teacher's Management Application</h3>
 		<ul class="navbar-nav">
